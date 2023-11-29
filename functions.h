29ee90e693e71e273645f9d3b313eb8d45482d69
Chosen808 <learnkingdom@gmail.com>
2023-11-29 18:34:33 -0400

    Commit 291123 1834

--- functions.h
+++ functions.h
@@ -1,6 +1,7 @@
 #pragma
 #include <stdio.h>
 
+// This function is use to initialize int, float, doubles etc from string input. Edit to suite
 void getInputValuesFromString (char string[], int input1, int input2, int input3)
 {
     sscanf (string, "%d %d %d", &input1, &input2, &input3);
