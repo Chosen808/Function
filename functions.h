a25d171f37a5d095c2325e8ccfa59c0f514616b2
Chosen808 <learnkingdom@gmail.com>
2023-11-29 16:31:30 -0400

    Commit 291123 1631

--- functions.h
+++ functions.h
@@ -1,7 +1,7 @@
 #pragma
 #include <stdio.h>
 
-void getInputValuesFromString (char string, int input1, int input2, int input3)
+void getInputValuesFromString (char string[], int input1, int input2, int input3)
 {
-    sscanf (string, %d %d %d, &input1, &input2, &input3);
+    sscanf (string, "%d %d %d", &input1, &input2, &input3);
 }
